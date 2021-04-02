1af1d5388bbce716a86d7945c0b088b4de5398bc
Rubens Pereira do Nascimento <rubens@wsac.com.br>
2021-04-01 21:30:58 -0300

    test: adiciona testes para os novos métodos

--- listas/test_lista_ligada.py
+++ listas/test_lista_ligada.py
@@ -42,37 +42,15 @@ class TestEstaVazia(TestListaLigada):
         self.assertFalse(self.lista.esta_vazia())
 
 
-class TestExisteElemento(TestListaLigada):
-    def test_sem_nos(self):
-        self.assertFalse(self.lista.existe_elemento(10))
-
-    def test_sem_elemento(self):
-        self.lista.inserir_elemento(1)
-        self.lista.inserir_elemento(2)
-        self.lista.inserir_elemento(3)
-        self.assertFalse(self.lista.existe_elemento(4))
-        self.assertFalse(self.lista.existe_elemento(10))
-
-    def test_com_elemento(self):
-        self.lista.inserir_elemento(10)
-        self.lista.inserir_elemento(20)
-        self.lista.inserir_elemento(30)
-        self.lista.inserir_elemento(40)
-        self.assertTrue(self.lista.existe_elemento(10))
-        self.assertTrue(self.lista.existe_elemento(20))
-        self.assertTrue(self.lista.existe_elemento(30))
-        self.assertTrue(self.lista.existe_elemento(40))
-
-
 class TestObterNo(TestListaLigada):
     def test_sem_nos(self):
-        self.assertIsNone(self.lista.obter_no(1))
+        self.assertIsNone(self.lista.obter_no(0))
 
     def test_sem_posicao(self):
         self.lista.inserir_elemento(1)
         self.lista.inserir_elemento(2)
         self.lista.inserir_elemento(3)
-        self.assertIsNone(self.lista.obter_no(4))
+        self.assertIsNone(self.lista.obter_no(3))
         self.assertIsNone(self.lista.obter_no(10))
 
     def test_com_posicao(self):
@@ -80,24 +58,68 @@ class TestObterNo(TestListaLigada):
         self.lista.inserir_elemento(20)
         self.lista.inserir_elemento(30)
         self.lista.inserir_elemento(40)
+        self.assertIsNotNone(self.lista.obter_no(0))
         self.assertIsNotNone(self.lista.obter_no(1))
         self.assertIsNotNone(self.lista.obter_no(2))
         self.assertIsNotNone(self.lista.obter_no(3))
-        self.assertIsNotNone(self.lista.obter_no(4))
 
 
 class TestObterElemento(TestListaLigada):
     def test_sem_nos(self):
-        self.assertIsNone(self.lista.obter_elemento(1))
+        self.assertIsNone(self.lista.obter_elemento(0))
 
     def test_sem_posicao(self):
         self.lista.inserir_elemento(1)
         self.lista.inserir_elemento(2)
         self.lista.inserir_elemento(3)
-        self.assertIsNone(self.lista.obter_elemento(4))
+        self.assertIsNone(self.lista.obter_elemento(3))
         self.assertIsNone(self.lista.obter_elemento(10))
 
     def test_com_posicao(self):
+        self.lista.inserir_elemento(10)
+        self.lista.inserir_elemento(20)
+        self.lista.inserir_elemento(30)
+        self.lista.inserir_elemento(40)
+        self.assertEqual(10, self.lista.obter_elemento(0))
+        self.assertEqual(20, self.lista.obter_elemento(1))
+        self.assertEqual(30, self.lista.obter_elemento(2))
+        self.assertEqual(40, self.lista.obter_elemento(3))
+
+
+class TestContem(TestListaLigada):
+    def test_sem_nos(self):
+        self.assertFalse(self.lista.contem(10))
+
+    def test_sem_elemento(self):
+        self.lista.inserir_elemento(1)
+        self.lista.inserir_elemento(2)
+        self.lista.inserir_elemento(3)
+        self.assertFalse(self.lista.contem(4))
+        self.assertFalse(self.lista.contem(10))
+
+    def test_com_elemento(self):
+        self.lista.inserir_elemento(10)
+        self.lista.inserir_elemento(20)
+        self.lista.inserir_elemento(30)
+        self.lista.inserir_elemento(40)
+        self.assertTrue(self.lista.contem(10))
+        self.assertTrue(self.lista.contem(20))
+        self.assertTrue(self.lista.contem(30))
+        self.assertTrue(self.lista.contem(40))
+
+
+class TestIndice(TestListaLigada):
+    def test_sem_nos(self):
+        self.assertEqual(-1, self.lista.indice(10))
+
+    def test_sem_elemento(self):
+        self.lista.inserir_elemento(1)
+        self.lista.inserir_elemento(2)
+        self.lista.inserir_elemento(3)
+        self.assertEqual(-1, self.lista.indice(4))
+        self.assertEqual(-1, self.lista.indice(10))
+
+    def test_com_elemento(self):
         self.lista.inserir_elemento(10)
         self.lista.inserir_elemento(20)
         self.lista.inserir_elemento(30)
@@ -106,3 +128,40 @@ class TestObterElemento(TestListaLigada):
         self.assertEqual(1, self.lista.indice(20))
         self.assertEqual(2, self.lista.indice(30))
         self.assertEqual(3, self.lista.indice(40))
+
+
+class TestInserirPosicao(TestListaLigada):
+    def test_posicao_maior(self):
+        self.lista.inserir_posicao(0, 1);
+        self.assertEqual(0, self.lista.indice(1))
+        self.assertEqual(1, self.lista.tamanho)
+
+        self.lista.inserir_posicao(10, 2);
+        self.assertEqual(1, self.lista.indice(2))
+        self.assertEqual(2, self.lista.tamanho)
+
+    def test_primeira_posicao(self):
+        self.lista.inserir_elemento(1)
+        self.lista.inserir_elemento(2)
+        self.lista.inserir_elemento(3)
+        self.lista.inserir_elemento(4)
+        self.lista.inserir_posicao(0, 0)
+        self.assertEqual(0, self.lista.indice(0))
+        self.assertEqual(1, self.lista.indice(1))
+        self.assertEqual(2, self.lista.indice(2))
+        self.assertEqual(3, self.lista.indice(3))
+        self.assertEqual(4, self.lista.indice(4))
+        self.assertEqual(5, self.lista.tamanho)
+
+    def test_no_meio(self):
+        self.lista.inserir_elemento(1)
+        self.lista.inserir_elemento(2)
+        self.lista.inserir_elemento(3)
+        self.lista.inserir_elemento(4)
+        self.lista.inserir_posicao(3, 10)
+        self.assertEqual(0, self.lista.indice(1))
+        self.assertEqual(1, self.lista.indice(2))
+        self.assertEqual(2, self.lista.indice(3))
+        self.assertEqual(3, self.lista.indice(10))
+        self.assertEqual(4, self.lista.indice(4))
+        self.assertEqual(5, self.lista.tamanho)
